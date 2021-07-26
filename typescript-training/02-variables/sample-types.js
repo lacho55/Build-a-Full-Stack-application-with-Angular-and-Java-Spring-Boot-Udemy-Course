9c6c13422cf59e9e9cf6d983102360c069107893
Lachezar Peshterliev <lachezar.peshterliev@gmail.com>
2021-07-26 14:34:36 +0300

    Adding working files

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,4 @@
+var found = true;
+var grade = 88.6;
+var firstName = 'Lachkata';
+var lastName = 'sample text';
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,4 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = 'Lachkata';
+let lastName: string = 'sample text';
